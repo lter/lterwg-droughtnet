693845ada42a27878a33397f4e8a910c874696b8
Timothy Ohlert <49884319+tim-ohlert@users.noreply.github.com>
2022-12-29 10:41:53 -0700

    Added RM index from Bondaruk 2022

--- Scripts/duration_lag_analyses_10-14-2022.R
+++ Scripts/duration_lag_analyses_10-14-2022.R
@@ -14,51 +14,18 @@ Site_Elev.Disturb <- read.csv("C:/Users/ohler/Dropbox/IDE MS_Single year extreme
 
 data.anpp <- anpp_ppt_map%>%
   left_join( Site_Elev.Disturb, by = "site_code")%>%
-  subset(habitat.type == "Grassland" | habitat.type == "Shrubland")%>%
-  subset(ppt.1 >0 &ppt.2 >0 &ppt.3 >0 &ppt.4 >0 )#%>%
-#ddply(.(ste_code, year, trt, ppt.1, ppt.2, ppt.3, ppt.4))
+  subset(habitat.type == "Grassland" | habitat.type == "Shrubland")#%>%
+  #subset(ppt.1 ==0 |ppt.2 ==0 |ppt.3 ==0 |ppt.4 ==0 )
+
+anpp.mean <- anpp_ppt_map%>%
+  #left_join( Site_Elev.Disturb, by = "site_code")%>%
+  #subset(habitat.type == "Grassland" | habitat.type == "Shrubland")%>%
+  subset(trt == "Control")%>%
+  ddply(.(site_code, year),function(x)data.frame(mass = mean(x$mass)))%>% #summarizes controls for each year
+  ddply(.(site_code),function(x)data.frame(mean.mass = mean(x$mass), n_years = length(x$mass)))#summarizes controls across years
 
 
-#mod.1 <- lmer(mass~ppt.1 + (1|site_code), data=anpp_ppt_map)
-#mod.2 <- lmer(mass~ppt.1 + ppt.2 + (1|site_code), data=anpp_ppt_map)
-#mod.3 <- lmer(mass~ppt.1 + ppt.2 + ppt.3 + (1|site_code), data=anpp_ppt_map)                
-#mod.4 <- lmer(mass~ppt.1 + ppt.2 + ppt.3 + ppt.4 + (1|site_code), data=anpp_ppt_map)                
-#AIC(mod.1, mod.2, mod.3, mod.4)
-
-#summary(mod.1)
-#summary(mod.2)
-#summary(mod.3)
-#summary(mod.4)
-#visreg(mod.1)
-
-
-
-#REMOVING FOREST SITES (Will leave this data to Rich Phillips)
-#data.noforest<-anpp_ppt_map %>%
-#  dplyr::filter(!habitat.type %in% c('Forest','Forest understory'))
-#length(unique(data.noforest$site_code)) #120 (16 sites removed)
-#setdiff(data.all$site_code,data.noforest$site_code)
-# [1] "bamboo.cn"    "bivensarm.us" "cmss.us"      "elizwood.us"  "gigante.pa"   "horizon.cr"  
-#[7] "hubbard.us"   "jilpanger.au" "kranz.de"     "p12.pa"       "p13.pa"       "prades.es"   
-#[13] "sevforest.us" "sherman.pa"   "thompson.us"  "wayqecha.pe" 
-#NOTE: Prades didn't follow IDE protocols, but also happens to be a forest site
-
-#Removing sites here that DO NOT report ANPP or are outside of ANPP range for biome listed in Fahey & Knapp 2007:
-#data.anpp<-data.noforest[which(data.noforest$site_code!="lcnorth.cl" #Doesn't report ANPP
-#                               &data.noforest$site_code!="lcsouth.cl" #Doesn't report ANPP
-#                               &data.noforest$site_code!="qdtnorth.cl" #Doesn't report ANPP
-#                               &data.noforest$site_code!="qdtsouth.cl" #Doesn't report ANPP
-#                               &data.noforest$site_code!="neudamm.na" #Doesn't report ANPP, also no weather info
- #                              &data.noforest$site_code!="ebro.es" #ANPP outside range for biome
-#                               &data.noforest$site_code!="garraf.es" #Did not follow protocols: Not using for drought plots, only using control plots for this site
-#                               &data.noforest$site_code!="brandjberg.dk" #Did not follow protocols: Not using for drought plots, only using control plots for this site
-#                               &data.noforest$site_code!="ethadb.au" #ANPP outside range for biome
-#                               &data.noforest$site_code!="ethadn.au" #ANPP outside range for biome
-#                               &data.noforest$site_code!="swift.ca" #did not follow IDE protocols (drought plots did not exclude water- see Jillian email)
-                               
-#),] 
-
-length(unique(data.anpp$site_code)) #108
+length(unique(data.anpp$site_code)) #110
 
 #Only using sites with >= 2 reps for drought and >=1 rep for control
 #From Mendy (11-08-2021):
@@ -100,7 +67,7 @@ Plot.trt.ct2<-gather(Plottrt_wide1,trt,Plot.count,Drought:Control)
 data.anpp1<-merge(data.anpp,Plot.trt.ct2,by=c("site_code","trt","year"))
 
 setdiff(data.anpp$site_code,data.anpp1$site_code) #no sites eliminated here
-length(unique(data.anpp1$site_code)) #1oo
+length(unique(data.anpp1$site_code)) #1o
 
 
 controls <- data.anpp1%>%
@@ -200,7 +167,7 @@ visreg2d(winning.mod.lmer, "drtsev.1", "drtsev.2", plot.type="gg", col = c("red"
 
 
 
-#####Can I remoake stitches plots by year and plot drought severity and response by year?
+#####Can I remake stitches plots by year and plot drought severity and response by year?
 subset(data.anpp.summary,n_treat_years ==1)%>%
 dplyr::mutate(site_code = fct_reorder(site_code, desc(anpp_response))) %>%
 ggplot(  aes(site_code, anpp_response))+
@@ -300,11 +267,20 @@ mod <- lmer(anpp_response~drtsev.1 + (1|site_code), data = subset(data.anpp2,n_t
 summary(mod)
 r.squaredGLMM(mod)
 
+temp <- subset(data.anpp2,n_treat_years >=1 & n_treat_years <= 4)
+mod <- lmer(anpp_response~drtsev.1 * n_treat_years + (1|site_code), data = temp)
+summary(mod)
+r.squaredGLMM(mod)
+visreg(mod)
+
 ###Some numbers on the data
 length(unique(subset(data.anpp.summary, n_treat_years == 1)$site_code))
 length(unique(subset(data.anpp.summary, n_treat_years == 2)$site_code))
 length(unique(subset(data.anpp.summary, n_treat_years == 3)$site_code))
 length(unique(subset(data.anpp.summary, n_treat_years == 4)$site_code))
+
+
+
 
 
 ##############################
@@ -356,3 +332,34 @@ summary(mod.3)
 r.squaredGLMM(mod.3)
 
 visreg(mod.2)
+
+
+
+####Do some stuff with the RM index from Bondaruk 2022
+
+anpp.RM <- data.anpp%>%
+          ddply(.(site_code, trt, year, n_treat_years, habitat.type),function(x)data.frame(
+            mass = mean(x$mass),
+            ppt.1 = mean(x$ppt.1),
+            #ppt.2 = mean(x$ppt.2),
+            #ppt.3 = mean(x$ppt.3),
+            #ppt.4 = mean(x$ppt.4),
+            MAP = mean(x$precip)
+          ))%>%
+          left_join(anpp.mean, by = "site_code")%>%
+          pivot_wider(names_from = "trt", values_from = c("mass", "ppt.1"))
+
+anpp.RM$RM <- ((anpp.RM$mass_Control-anpp.RM$mass_Drought) / anpp.RM$mean.mass)/((anpp.RM$ppt.1_Control-anpp.RM$ppt.1_Drought) / anpp.RM$MAP)
+
+
+ggplot(subset(anpp.RM,n_treat_years >= 1 &n_treat_years <=4), aes(as.factor(n_treat_years), RM))+
+  geom_boxplot()+
+  theme_base()
+  
+  
+ggplot(subset(anpp.RM,n_treat_years >= 1 &n_treat_years <=4), aes(MAP, RM, color = habitat.type))+
+  facet_wrap(~n_treat_years)+
+  geom_point()+
+  ylim(-5,5)+
+  theme_base()
+  
