73144944baa192dd7dc0cf27c4c2df1de792883c
Timothy Ohlert <49884319+tim-ohlert@users.noreply.github.com>
2023-02-08 18:11:01 -0700

    update

--- Scripts/ANPP_duration/duration_analyses_2023-01-10.R
+++ Scripts/ANPP_duration/duration_analyses_2023-01-10.R
@@ -111,7 +111,7 @@ data.anpp2$drtsev.4 <- ((data.anpp2$ppt.4-data.anpp2$map)/data.anpp2$map)
 
 
 data.anpp.summary <- data.anpp2%>%
-  ddply(.(site_code, year, drtsev.1,drtsev.2,drtsev.3,drtsev.4, n_treat_years),
+  ddply(.(site_code, year, drtsev.1,drtsev.2,drtsev.3,drtsev.4, n_treat_years, map),
         function(x)data.frame(
           anpp_response = mean(x$anpp_response),
           anpp_response.error = qt(0.975, df=length(x$habitat.type)-1)*sd(x$anpp_response, na.rm = TRUE)/sqrt(length(x$habitat.type)-1),
@@ -123,6 +123,10 @@ length(unique(data.anpp.summary$site_code))
 
 
 
+
+
+
+  
 #big.mod <- lmer(anpp_response~n_treat_days*drtsev.1 + (1|site_code), data = data.anpp.summary)
 #summary(big.mod)
 big.mod <- lme(anpp_response~n_treat_days*drtsev.1, random =~1|site_code, data = data.anpp.summary, correlation = corAR1())
@@ -163,12 +167,15 @@ visreg2d(big.mod, "n_treat_days","drtsev.1",  plot.type="gg", col = c("red", "wh
   theme_base()  
 
 
-ggplot(data.anpp.summary, aes(anpp_response, fill = as.factor(n_treat_years)))+
-  geom_histogram(aes(y=..density..),position = "identity", binwidth = .2)+
-  geom_density(alpha = 0.5, adjust = 5)+
+
+ggplot(data = data.anpp.summary, aes(x=n_treat_days, y=drtsev.1))+
+  geom_point(aes(fill = cut(anpp_response, 10), size = 3), color = "black", pch = 21, alpha = 0.9)+
+  scale_fill_brewer(palette = "Reds", direction = -1
+                    , drop = FALSE)+
+  geom_hline(yintercept = 0, linetype = "dashed")+
+  geom_vline(xintercept = 0, linetype = "dashed")+
+  
   theme_base()
-
-
 
 ##this bit tries testing the model residuals but it doesn't show anything
 temp.mod <- lmer(anpp_response~drtsev.1 + (1|site_code), data = data.anpp.summary)
@@ -185,13 +192,17 @@ visreg(mod.resid)
 ##############try model selection
 library(MASS)
 library(nlme)
+sandsite.filt<-read.csv("C:\\Users\\ohler\\Dropbox\\IDE MS_Single year extreme\\Data\\site_sand_from_soilgrid.csv")
+data.anpp.summary <- data.anpp.summary%>%
+  left_join(sandsite.filt, by = "site_code")
+
 #Backward model selection - skipping backward in favor of forward since backward likes the maximal model for some ungodly reason
-lmFull <- lme(anpp_response~drtsev.1 * drtsev.2 * drtsev.3 , random = ~1|site_code, data=data.anpp.summary, method = "ML", na.action = na.exclude, correlation = corAR1())
+lmFull <- lme(anpp_response~drtsev.1 * drtsev.2 * drtsev.3, random = ~1|site_code, data=data.anpp.summary, method = "ML", na.action = na.exclude, correlation = corAR1())
 
 
-stepAIC(lmFull, scope = list(upper = lmFull,
-                            lower = ~1),
-        trace = F)
+#stepAIC(lmFull, scope = list(upper = lmFull,
+#                            lower = ~1),
+#        trace = F)
 
 lmNull <- lme(anpp_response~1, random = ~ 1 |site_code, data = data.anpp.summary, method = "ML",  na.action=na.exclude, correlation = corAR1())
 
@@ -226,16 +237,6 @@ visreg2d(winning.mod, "drtsev.2", "drtsev.3", plot.type="gg", col = c("red", "wh
 
 
 
-# Create a sequence of incrementally increasing (by 0.3 units) values for both wt and hp
-xgrid <-  seq(min(data.anpp.summary$drtsev.1), max(data.anpp.summary$drtsev.1), 0.3)
-ygrid <-  seq(min(data.anpp.summary$drtsev.2), max(data.anpp.summary$drtsev.2), 0.3)
-# Generate a dataframe with every possible combination of wt and hp
-data.fit <-  expand.grid(drtsev.1 = xgrid, drtsev.2 = ygrid)
-# Feed the dataframe into the loess model and receive a matrix output with estimates of
-# acceleration for each combination of wt and hp
-mtrx3d <-  predict(winning.mod#, newdata = data.fit
-                   )
-plot(mtrx3d)
 
 ggplot(data = data.anpp.summary, aes(x=drtsev.1, y=drtsev.2))+
   geom_point(aes(fill = anpp_response, size = 2), color = "black", pch = 21)+
@@ -260,12 +261,12 @@ ggplot(data = data.anpp.summary, aes(x=drtsev.1, y=drtsev.2))+
 mtrx3d[1:4, 1:4]
 
 #Backward model selection - skipping backward in favor of forward since backward likes the maximal model for some ungodly reason
-lmFull <- lm(anpp_response~drtsev.1 * drtsev.2 * drtsev.3 , random = ~1|site_code, data=subset(data.anpp.summary, n_treat_years == 4))
+lmFull <- lm(anpp_response~drtsev.1 * drtsev.2 * drtsev.3 + map + sand_mean, data=subset(data.anpp.summary, n_treat_years == 3))
 
 
-stepAIC(lmFull, scope = list(upper = lmFull,
-                             lower = ~1),
-        trace = F)
+#stepAIC(lmFull, scope = list(upper = lmFull,
+##                             lower = ~1),
+#        trace = F)
 
 lmNull <- lm(anpp_response~1,  data = subset(data.anpp.summary, n_treat_years == 3),  na.action=na.exclude)
 
@@ -279,7 +280,7 @@ stepAIC(lmNull, scope = list(upper = lmFull,
 
 tempdf <-subset(data.anpp.summary, n_treat_years == 3)
 winning.mod <- lm(anpp_response ~ drtsev.1 + drtsev.2 + drtsev.3 + 
-                    drtsev.1:drtsev.2 + drtsev.2:drtsev.3, data = tempdf)
+                    map + drtsev.1:drtsev.2 + drtsev.2:drtsev.3, data = tempdf)
 summary(winning.mod)
 
 #winning.mod.lmer <- lmer(anpp_response ~ drtsev.1 + drtsev.2 + drtsev.3 + drtsev.1:drtsev.2 +      drtsev.2:drtsev.3 + drtsev.1:drtsev.3 +(1|site_code),data = data.anpp.summary)
@@ -293,27 +294,8 @@ visreg2d(winning.mod, "drtsev.1", "drtsev.2", plot.type="gg", col = c("red", "wh
   #geom_vline(xintercept = 0, linetype = "dashed")+
   theme_base()  
 
-ggplot(data = subset(data.anpp.summary, n_treat_years == 3), aes(x=drtsev.1, y=drtsev.2))+
-  geom_point(aes(fill = anpp_response, size = 2), color = "black", pch = 21)+
-  scale_fill_gradient2(
-    low = "red",
-    mid = "orange",
-    high = "white",
-    midpoint = 0,
-    #space = "Lab",
-    #na.value = "grey50",
-    #guide = "colourbar",
-    #aesthetics = "fill"
-  )+
-  geom_hline(yintercept = 0, linetype = "dashed")+
-  geom_vline(xintercept = 0, linetype = "dashed")+
-  
-  theme_base()
 
 
-geom_raster(aes(fill = cut(z, zCuts))) +
-  scale_fill_brewer(palette = "RdBu"
-                    , drop = FALSE)
 
 ggplot(data = subset(data.anpp.summary, n_treat_years == 3), aes(x=drtsev.1, y=drtsev.2))+
   geom_point(aes(fill = cut(anpp_response, 5), size = 3), color = "black", pch = 21, alpha = 0.9)+
@@ -420,4 +402,25 @@ ggplot(anpp.avg, aes(avg.drtsev, lrr_avg))+
   geom_hline(yintercept = 0)+
   geom_vline(xintercept = 0)+
   theme_base()
+
+
+##################################
+data.anpp.year <- data.anpp.summary%>%
+  ddply(.(n_treat_years),
+        function(x)data.frame(
+          anpp_response = mean(x$anpp_response),
+          anpp_response.error = qt(0.975, df=length(x$site_code)-1)*sd(x$anpp_response, na.rm = TRUE)/sqrt(length(x$site_code)-1)
+        ))
+
+ggplot(data.anpp.year, aes(as.factor(n_treat_years), anpp_response))+
+  geom_pointrange(aes(ymin = anpp_response-anpp_response.error, ymax = anpp_response+anpp_response.error))+
+  ylim(-1, 0)+
+  geom_hline(yintercept = 0)+
+  theme_base()
+
+mod <- lme(anpp_response~as.factor(n_treat_years), random = ~1|site_code, data = data.anpp.summary,)
+summary(mod)
+
+
+
 
