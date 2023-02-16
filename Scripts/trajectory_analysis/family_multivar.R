b96d178cc81c7cb3d2ec407d1b6e723ad2e05ccb
njlyon0 <nicholasjlyon@gmail.com>
2023-02-16 14:30:11 -0500

    Empty script for family-level multivariate analysis

---
+++ Scripts/trajectory_analysis/family_multivar.R
@@ -0,0 +1,19 @@
+## ------------------------------------------------------- ##
+# Functional Group Trajectory Analysis
+## ------------------------------------------------------- ##
+
+# Purpose:
+## Perform trajectory analysis on non-species abundances
+## Specifically, on functional group abundance & family abundance
+
+## -------------------------------------- ##
+# Housekeeping ----
+## -------------------------------------- ##
+# Load libraries
+# install.packages("librarian")
+librarian::shelf(tidyverse, RRPP, NCEAS/scicomptools, supportR)
+
+# Clear environment
+rm(list = ls())
+
+# End ----
