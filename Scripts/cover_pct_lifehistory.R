6a0e87b1d6c5f22322c9a18fb3afb8d9d58550c8
Meghan Avolio <meghan.avolio@gmail.com>
2023-03-21 12:59:32 -0400

    calculated on yr 1 control data. probably not going to keep this appraoch.

--- Scripts/cover_pct_lifehistory.R
+++ Scripts/cover_pct_lifehistory.R
@@ -10,28 +10,26 @@ dat<-read.csv("cover_ppt_2023-02-06.csv") %>%
   mutate(replicate=paste(block, plot, subplot, sep="::"),
          lifeform2=ifelse(local_lifeform=="GRAMINOID", "GRASS", local_lifeform))
 
-#dropping datasets without pretreatment
-drop_no_pretrt<-dat %>% 
-  select(site_code, n_treat_years) %>% 
-  unique() %>% 
-  filter(n_treat_years==0) %>% 
-  select(-n_treat_years)
-
-dat2<-dat %>% 
-  right_join(drop_no_pretrt) %>% 
-  filter(trt=="Drought")
+# #dropping datasets without pretreatment
+# drop_no_pretrt<-dat %>% 
+#   select(site_code, n_treat_years) %>% 
+#   unique() %>% 
+#   filter(n_treat_years==0) %>% 
+#   select(-n_treat_years)
+# 
+#doing this for yr1 controls
+dat2<-dat %>%
+  filter(trt=="Control"&n_treat_years==1)
 
 totalcover<-dat2 %>% 
-  filter(n_treat_years==0) %>% 
   group_by(site_code, replicate) %>% 
   summarize(tot=sum(max_cover))
 
 pctLifeForm<-dat2 %>% 
-  filter(n_treat_years==0) %>% 
   group_by(site_code, replicate, lifeform2) %>% 
   summarize(sum=sum(max_cover)) %>% 
   pivot_wider(names_from = lifeform2, values_from = sum, values_fill = 0) %>% 
-  pivot_longer(FORB:CACTUS, names_to = "lifeform2", values_to="sum") %>% 
+  pivot_longer(FORB:MOSS, names_to = "lifeform2", values_to="sum") %>% 
   filter(lifeform2=="GRASS") %>% 
   left_join(totalcover) %>% 
   mutate(PGrass=(sum/tot)*100) %>% 
@@ -39,7 +37,6 @@ pctLifeForm<-dat2 %>%
   summarize(PctGrass=mean(PGrass))
 
 pctLifehistory<-dat2 %>% 
-  filter(n_treat_years==0) %>% 
   group_by(site_code, replicate, local_lifespan) %>% 
   summarize(sum=sum(max_cover)) %>% 
   filter(site_code!="lygraold.no"&site_code!="lygraint.no"&site_code!="purdue.us"&site_code!="eea.br") %>% 
@@ -53,4 +50,4 @@ pctLifehistory<-dat2 %>%
   summarize(PctAnnual=mean(PAnn)) %>% 
   full_join(pctLifeForm)
 
-write.csv(pctLifehistory, "C:\\Users\\mavolio2\\Dropbox\\Prc_LifeHistory.csv", row.names=F)
+write.csv(pctLifehistory, "C:\\Users\\mavolio2\\Dropbox\\Prc_LifeHistory_Yr1Controls.csv", row.names=F)
