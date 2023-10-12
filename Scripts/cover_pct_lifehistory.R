01a29f7c0295201c97c5e0cd24c6d05385aedc41
Meghan Avolio <meghan.avolio@gmail.com>
2023-10-12 14:40:35 -0400

    now estimating precent annaul percent grass based on control data for all years.

--- Scripts/cover_pct_lifehistory.R
+++ Scripts/cover_pct_lifehistory.R
@@ -1,5 +1,6 @@
 ###Code to for each site what % grass/forb and annual/perennial species
 ###code by Meghan Avolio, Feb 8, 2023
+###Updataing files Oct 11, 2023
 
 library(tidyverse)
 #library(codyn)
@@ -7,9 +8,12 @@ library(tidyverse)
 setwd("C:\\Users\\mavolio2\\Dropbox\\IDE (1)\\data_processed")
 setwd("E:Dropbox\\IDE (1)\\data_processed")
 
-dat<-read.csv("cover_ppt_2023-05-10.csv") %>% 
+dat<-read.csv("cover_ppt_2023-10-12.csv") %>% 
   mutate(replicate=paste(block, plot, subplot, sep="::"),
-         lifeform2=ifelse(local_lifeform=="GRAMINOID", "GRASS", local_lifeform))
+         lifeform2=ifelse(local_lifeform=="GRAMINOID"|local_lifeform=="Grass", "GRASS", local_lifeform))
+
+
+# Calculating by plots in year 0 only -------------------------------------
 
 #dropping datasets without pretreatment
 drop_no_pretrt<-dat %>%
@@ -37,6 +41,11 @@ pctLifeForm<-dat2 %>%
   group_by(site_code) %>% 
   summarize(PctGrass=mean(PGrass))
 
+test<-dat2 %>% 
+  filter(lifeform2=="Grass") %>% 
+  select(site_code, Taxon, local_lifeform, local_lifespan, local_provenance, N.fixer, ps_path, functional_group) %>% 
+  unique()
+
 pctLifehistory<-dat2 %>% 
   group_by(site_code, replicate, local_lifespan) %>% 
   summarize(sum=sum(max_cover)) %>% 
@@ -50,4 +59,60 @@ pctLifehistory<-dat2 %>%
   summarize(PctAnnual=mean(PAnn)) %>% 
   full_join(pctLifeForm)
 
-write.csv(pctLifehistory, "community_comp\\Prc_LifeHistory_July2023.csv", row.names=F)
+write.csv(pctLifehistory, "community_comp\\Prc_LifeHistory_Oct2023.csv", row.names=F)
+
+
+# Calculating for controls over all years ---------------------------------
+
+#dropping treated plots
+controls<-dat %>%
+  select(site_code, trt) %>%
+  unique() %>%
+  filter(trt=='Control') %>%
+  select(-trt)
+
+#doing this on contols data
+datControl<-dat %>%
+  filter(trt=='Control')
+
+totalcover_control<-datControl %>% 
+  group_by(site_code, replicate) %>% 
+  summarize(tot=sum(max_cover))
+
+pctLifeForm_contorl<-datControl %>% 
+  group_by(site_code, replicate, lifeform2) %>% 
+  summarize(sum=sum(max_cover)) %>% 
+  pivot_wider(names_from = lifeform2, values_from = sum, values_fill = 0) %>% 
+  pivot_longer(FORB:MOSS, names_to = "lifeform2", values_to="sum") %>% 
+  filter(lifeform2=="GRASS") %>% 
+  left_join(totalcover_control) %>% 
+  mutate(PGrass=(sum/tot)*100) %>% 
+  group_by(site_code) %>% 
+  summarize(PctGrass=mean(PGrass))
+
+pctLifehistory_control<-datControl %>% 
+  group_by(site_code, replicate, local_lifespan) %>% 
+  summarize(sum=sum(max_cover)) %>% 
+  filter(local_lifespan!="NULL") %>% 
+  pivot_wider(names_from = local_lifespan, values_from = sum, values_fill = 0) %>%   pivot_longer(ANNUAL:UNK, names_to = "local_lifespan", values_to="sum") %>% 
+  filter(local_lifespan=="ANNUAL") %>% 
+  left_join(totalcover_control) %>% 
+  mutate(PAnn=(sum/tot)*100) %>% 
+  group_by(site_code) %>% 
+  summarize(PctAnnual=mean(PAnn)) %>% 
+  full_join(pctLifeForm_contorl)
+
+write.csv(pctLifehistory_control, "community_comp\\Prc_LifeHistory_Controls_Oct2023.csv", row.names=F)
+
+
+
+combine<-pctLifehistory_control %>% 
+  left_join(pctLifehistory)
+
+
+ggplot(data=combine, aes(x=CPctAnnual, y=PctAnnual))+
+  geom_point()
+
+
+ggplot(data=pctLifehistory_control, aes(x=CPctAnnual))+
+  geom_histogram()
