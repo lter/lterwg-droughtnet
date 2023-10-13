030ad150397a529195b67b0072f4f356df4d0707
njlyon0 <nicholasjlyon@gmail.com>
2023-10-13 14:55:17 -0400

    Saving IPCC region extraction script (just the start bit)

---
+++ Scripts/extract_ipcc.R
@@ -0,0 +1,22 @@
+## ------------------------------------------------------- ##
+                    # Identify IPCC Regions
+## ------------------------------------------------------- ##
+
+# Purpose:
+## Strip out IPCC regions for DroughtNet Sites
+## See this paper for more information:
+### https://essd.copernicus.org/articles/12/2959/2020/essd-12-2959-2020.html
+
+## -------------------------------------- ##
+# Housekeeping ----
+## -------------------------------------- ##
+# Load libraries
+# install.packages("librarian")
+librarian::shelf(tidyverse)
+
+# Clear environment
+rm(list = ls())
+
+
+
+# End ----
