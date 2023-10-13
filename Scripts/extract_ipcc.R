e4af3a99e4f5ec25e4f72382c1a80981914ac2c1
njlyon0 <nicholasjlyon@gmail.com>
2023-10-13 14:57:44 -0400

    Added steps for prepping the coordinate information for use in extracting IPCC stuff

--- Scripts/extract_ipcc.R
+++ Scripts/extract_ipcc.R
@@ -8,7 +8,7 @@
 ### https://essd.copernicus.org/articles/12/2959/2020/essd-12-2959-2020.html
 
 ## -------------------------------------- ##
-# Housekeeping ----
+            # Housekeeping ----
 ## -------------------------------------- ##
 # Load libraries
 # install.packages("librarian")
@@ -17,6 +17,25 @@ librarian::shelf(tidyverse)
 # Clear environment
 rm(list = ls())
 
+# Read in data
+drought_df <- read.csv(file = file.path("Data", "Site_Elev-Disturb.csv"))
+
+# Glimpse it
+dplyr::glimpse(drought_df)
+
+## -------------------------------------- ##
+# Prep Coordinate Object ----
+## -------------------------------------- ##
+
+# Begin by making an ultra-simple data object with **only** coordinate info
+coords <- drought_df %>%
+  # Keep only coordinate columns
+  dplyr::select(latitud, longitud) %>%
+  # Keep only one row per coordinate
+  dplyr::distinct()
+
+# Check structure
+dplyr::glimpse(coords)
 
 
 # End ----
