81bd6735bd44d0b45581019d4da2128f6f405188
Timothy Ohlert <49884319+tim-ohlert@users.noreply.github.com>
2024-07-19 12:48:47 -0600

    new data

--- Scripts/make-n_treat_years.R
+++ Scripts/make-n_treat_years.R
@@ -5,12 +5,12 @@ library(plyr)
 
 
 
-data.anpp <- read.csv("C:/Users/ohler/Dropbox/IDE MS_Single year extreme/Data/full_biomass_03-14-2024.csv")%>%
+data.anpp <- read.csv("C:/Users/ohler/Dropbox/IDE MS_Single year extreme/Data/full_biomass_07-18-2024.csv")%>%
               dplyr::select(site_code, year, n_treat_days)%>%
               unique()
 data.anpp$n_treat_days <- as.numeric(data.anpp$n_treat_days)
 
-data.cover <- read.csv("C:/Users/ohler/Dropbox/IDE/data_raw/full_cover_2023-10-25.csv")%>%
+data.cover <- read.csv("C:/Users/ohler/Dropbox/IDE/data_raw/full_cover_2024-04-16.csv")%>%
               dplyr::select(site_code, year, n_treat_days)%>%
               unique()%>%
               subset(n_treat_days != "NULL")#marcdrt.ar has some null values for unknown reasons
@@ -92,4 +92,5 @@ subset(combfin, is.na(n_treat_years) == TRUE)
 
 
 
-write.csv(combfin, "C:/Users/ohler/Dropbox/IDE/data_processed/IDE_treatment_years_2024-03-14.csv")
+write.csv(combfin, "C:/Users/ohler/Dropbox/IDE/data_processed/IDE_treatment_years_2024-07-18.csv")
+
