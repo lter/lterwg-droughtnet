0e4b53174c1e6cb08f9677fe343ae3d7448462bf
Timothy Ohlert <49884319+tim-ohlert@users.noreply.github.com>
2024-12-16 16:15:20 -0700

    update

--- Scripts/soil_data_harmonization.R
+++ Scripts/soil_data_harmonization.R
@@ -18,11 +18,12 @@ key$dup <- duplicated(dplyr::select(key, source, tidy_name) )
 
 harmony_df <- ltertools::harmonize(key = key, raw_folder = "C:/Users/ohler/Dropbox/IDE_data_May 2018/Soil Reports/Tidy version", 
                                    data_format = "csv", quiet = TRUE)%>%
-              subset(site_code != "")
+              subset(site_code != "")%>%
+                      dplyr::select(-c("site", "treatment", "x", "x_1", "x_2","x_3", "x_mo", "sample_id", "fert", "sample_density_g_ml", "exch_acidity_cmol_l", "total_cations_cmol_l", "acid_sat", "lab", "sample", "h", "c_org_n", "conductividad_s", "x2000_50_m", "x50_20_m", "x20_2_m", "x_2_m", "Sikora")) 
 
 
 
-write.csv(harmony_df, "C:/Users/ohler/Dropbox/IDE/data_processed/IDE_soil_2024-10-18.csv")
+write.csv(harmony_df, "C:/Users/ohler/Dropbox/IDE/data_processed/IDE_soil_2024-12-16.csv")
 #data <- harmonize(key = NULL, raw_folder = "C:/Users/ohler/Dropbox/IDE_data_May 2018/Soil Reports/Tidy version", data_format = "xlsx")
 
 ?harmonize
