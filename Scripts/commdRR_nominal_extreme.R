4a85a1e8c8993e27356e980f0f10c375523a9ad0
mavolio <meghan.avolio@jhu.edu>
2025-03-27 09:24:17 -0400

    making figure prettier

--- Scripts/commdRR_nominal_extreme.R
+++ Scripts/commdRR_nominal_extreme.R
@@ -7,14 +7,19 @@ rr3<-RR2 %>%
   left_join(nominal) %>% 
   group_by(n_treat_years, measure, e.n) %>% 
   summarise(mrr=mean(RR), se=(sd(RR))/sqrt(length(RR))) %>% 
-  na.omit()
+  na.omit() %>% 
+  filter(measure!='rank_change'&measure!='evenness_change')
 
-ggplot(data=rr3, aes(x=n_treat_years, y=mrr, color=e.n))+
-  geom_point()+
+labs<-c('richness_change'='Richness Change', 'gains'='Gains', 'losses'='Losses')
+
+fig<-ggplot(data=rr3, aes(x=n_treat_years, y=mrr, color=e.n))+
+  geom_point(size=3)+
   geom_errorbar(aes(ymin=mrr-se, ymax=mrr+se), width=0.25)+
-  facet_wrap(~measure)+
+  facet_wrap(~measure, labeller=labeller(measure=labs))+
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())+
   geom_hline(yintercept = 0)+
   ylab("Drought-Control Differences")+
   xlab('Year of Drought Treatment')+
-  scale_color_manual(name="Drought type", values=c('darkred', 'pink2'))
+  scale_color_manual(name="Drought type", values=c('orange2', 'gray'), labels=c('Extreme', 'Nominal'))
+
+ggsave('C:\\Users\\mavolio2\\Dropbox\\IDE (1)\\papers\\supplemental_fig_RR.pdf',fig, width=8, height=5, units='in' )
