40754299c0f1ccc85f24507bf2b0b6ae15eb7fb8
Timothy Ohlert <49884319+tim-ohlert@users.noreply.github.com>
2026-03-26 11:46:13 -0600

    now includes SOC

--- Scripts/extract_soilgrid.R
+++ Scripts/extract_soilgrid.R
@@ -61,7 +61,55 @@ sites$sand_0_60cm_weighted <- weighted_soil_mean(
 site_soil_data <- sites%>%
                   mutate(site_code = id)
 
-write.csv(site_soil_data, "C:/Users/ohler/Dropbox/IDE/data_processed/site_sand_from_soilgrid_2026-03-24.csv")
 
 
 
+
+
+
+# --- extract SOC by depth ---
+for (d in depths) {
+  soildat <- soil_world("soc", depth = d, stat = "mean")
+  soc_vals <- extract(soildat, sites_v)
+  
+  # name column based on depth
+  colname <- paste0("soc_0_", d, "cm")
+  sites[[colname]] <- soc_vals[, 2]
+}
+
+# --- layer metadata (for weighting) ---
+layer_info <- data.frame(
+  depth  = c(5, 15, 30, 60, 100),
+  top    = c(0, 5, 15, 30, 60),
+  bottom = c(5, 15, 30, 60, 100)
+)
+
+layer_info$thickness <- layer_info$bottom - layer_info$top
+layer_info
+
+
+
+weighted_soil_mean <- function(df, var_prefix, depths, thickness) {
+  cols <- paste0(var_prefix, "_0_", depths, "cm")
+  
+  values <- as.matrix(df[, cols])
+  weights <- thickness / sum(thickness)
+  
+  rowSums(
+    values * matrix(weights,
+                    nrow = nrow(values),
+                    ncol = length(weights),
+                    byrow = TRUE),
+    na.rm = TRUE
+  )
+}
+
+sites$soc_0_60cm_weighted <- weighted_soil_mean(
+  df = sites,
+  var_prefix = "soc",
+  depths = c(5, 15, 30, 60),
+  thickness = c(5, 10, 15, 30)
+)
+
+
+write.csv(site_soil_data, "C:/Users/ohler/Dropbox/IDE/data_processed/site_sand_soc_from_soilgrid_2026-03-26.csv")
